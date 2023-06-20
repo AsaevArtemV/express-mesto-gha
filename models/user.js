275362e160627dbcb473acce288471b17f2ed030
asaev <ArtemAV13@yandex.ru>
2023-06-20 18:31:16 +0300

    Second iteration 11

--- models/user.js
+++ models/user.js
@@ -17,11 +17,6 @@ const userSchema = new mongoose.Schema({
   avatar: {
     type: String,
     required: [true, 'Поле "name" должно быть заполнено'],
-    validate: {
-      // eslint-disable-next-line no-undef
-      validator: (v) => validator.isURL(v),
-      message: 'Некорректный URL',
-    }
   },
 });
 
